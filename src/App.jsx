65f4cb7a6ffe70ea1002246680edce3cf8778d99
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2025-01-24 15:12:08 +0530

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './template/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
      <h1>This project is created to undersand how to use git
        in VS code
      </h1>
---
+++ src/template/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{backgroundColor:"white" ,color:'royalblue'}}
+    >Header</div>
+  )
+}
+
+export default Header
